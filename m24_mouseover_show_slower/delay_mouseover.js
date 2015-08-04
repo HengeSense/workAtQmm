2862a1037d7cea514ace29592c17b482f057658b
AJ <735267452@qq.com>
2015-08-04 13:58:12 +0800

    mouseover delay 300ms show

--- m24_mouseover_show_slower/delay_mouseover.js
+++ m24_mouseover_show_slower/delay_mouseover.js
@@ -2,22 +2,23 @@ var g = $("#J_nav_site"),
 	attr = 'aj-mouseenter-timestamp';
 		
 	
-	g.find(".JCategoryChange li").on("mouseenter mouseleave", function (e) {
-		if (e.type === 'mouseenter') {
-			$(this).attr(attr, + new Date());
-		} else if (e.type === 'mouseleave') {
-			console.log( (+ new Date()) - parseInt($(this).attr(attr), 10));
-			$(this).removeAttr(attr);
-		}
-	});
-	
 		g.find(".JCategoryChange li").on("aj-delay-mouseover", {
-			attr ：'aj-mouseenter-timestamp'
-		}, function (event, fn) {
-			if (e.type === 'mouseenter') {
-				$(this).attr(attr, + new Date());
-			} else if (e.type === 'mouseleave') {
-				console.log( (+ new Date()) - parseInt($(this).attr(attr), 10));
-				$(this).removeAttr(attr);
-			}
-		});	
+			'attr' : 'aj-mouseenter-timestamp'
+		}, function (e, fn) {
+			$(this).on('mouseenter mouseleave', function (e) {
+				if (e.type === 'mouseenter') {
+					$(this).attr(attr, + new Date());
+				} else if (e.type === 'mouseleave') {
+					console.log( (+ new Date()) - parseInt($(this).attr(attr), 10));
+					$(this).removeAttr(attr);
+				}
+			});
+			$(this).on('mouseover', function () {
+				var that = this;
+				setTimeout(function () {
+					if ($(that).attr(attr)) {
+						fn(that);
+					}
+				}, 300);				
+			});
+		});
--- m24_mouseover_show_slower/m24_mouseover_delay300ms_show/index_new.js
+++ m24_mouseover_show_slower/m24_mouseover_delay300ms_show/index_new.js
@@ -2,22 +2,29 @@
 		attr = 'aj-mouseenter-timestamp';
     if (g.length > 0) {
         //滚动
-		g.find(".JCategoryChange li").on("mouseenter mouseleave", function (e) {
-			if (e.type === 'mouseenter') {
-				$(this).attr(attr, + new Date());
-			} else if (e.type === 'mouseleave') {
-				console.log( (+ new Date()) - parseInt($(this).attr(attr), 10));
-				$(this).removeAttr(attr);
-			}
-		});
-        g.find(".JCategoryChange li").on("mouseover", function () {
-			var that = this;
-			setTimeout(function () {
-				if ($(that).attr(attr)) {
-					toggle(that);
+		g.find(".JCategoryChange li").on("aj-delay-mouseover", {
+			'attr' : 'aj-mouseenter-timestamp'
+		}, function (e, fn) {
+			var attr = e.data.attr;
+			$(this).on('mouseenter mouseleave', function (e) {
+				if (e.type === 'mouseenter') {
+					$(this).attr(attr, + new Date());
+				} else if (e.type === 'mouseleave') {
+					console.log( (+ new Date()) - parseInt($(this).attr(attr), 10));
+					$(this).removeAttr(attr);
 				}
-			}, 300);
-        });
+			});
+			$(this).on('mouseover', function () {
+				var that = this;
+				setTimeout(function () {
+					if ($(that).attr(attr)) {
+						fn(that);
+					}
+				}, 300);				
+			});
+		});
+		//把发生delay mouseover 的事件句柄作为第一个参数, 返回的参数obj是当前对象
+		g.find(".JCategoryChange li").trigger("aj-delay-mouseover", toggle);
 		function toggle(obj) {
 			g.find(".JCategoryChange li").removeClass("cur")
             
