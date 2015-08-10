98e5ace299f995821b3648e1766f0cd24f07958a
AJ <735267452@qq.com>
2015-08-10 17:12:34 +0800

    解决滚动时导航的定位问题

--- m12_all_websites/index.js
+++ m12_all_websites/index.js
@@ -1,23 +1,52 @@
 (function () {
 	var w = {},
 		timer,
-		div = $('.aj-line-ceil'),
-		cid;
+		scrollTimer,
+		div = $('#J_nav_site .aj-line-ceil'),
+		cid,
+		blocks = $('#J_nav_site .aj-website-fenlei'),
+		isScrollFromClick = false;
+	resize();
 	$(window).on('resize', function () {
 		if (!timer) {
 			timer = setTimeout(function () {
-				w.width = $(window).width();
-				div.css('left', (w.width - 1050) / 2 - div.width() - 20 + 'px');
+				resize();
 				timer = 0;
 			}, 100);
+		}
+	});
+	$(window).on('scroll', function () {
+		if (!scrollTimer) {
+			scrollTimer = setTimeout(function () {
+				scroll();
+				scrollTimer = 0;
+			}, 200);
 		}
 	});
 	div.on('click', '.aj-li', function (e) {
 		e.preventDefault();
 		if ($(this).hasClass('aj-select')) return false;
 		cid = $(this).find('a').attr('href');
-		$(document.body).animate({'scrollTop' : $(cid).offset().top - 100 + 'px'});
+		var obj = $(cid);
+		isScrollFromClick = true;
+		$(document.body).animate({'scrollTop' : obj.offset().top + obj.height() / 2 - $(window).height() /2  + 'px'}, function () {
+			isScrollFromClick = false;
+		});
 		$(this).addClass('aj-select').siblings().removeClass('aj-select');
 	});
-
+	function resize() {
+		w.width = $(window).width();
+		div.css('left', (w.width - 1050) / 2 - div.width() - 20 + 'px');		
+	}
+	function scroll(){
+		if (isScrollFromClick) {
+			console.log(1);
+			return false; //不处理来自点击导航引起的页面滚动
+		}
+		blocks.each(function (index, obj) {
+			if (($(this).offset().top - $(document.body).scrollTop() < $(window).height() / 2) && ($(this).offset().top + $(this).height() - $(document.body).scrollTop() > $(window).height() /2)) {
+				div.find('.aj-li').eq(index).addClass('aj-select').siblings().removeClass('aj-select');
+			}
+		});
+	}
 }());
