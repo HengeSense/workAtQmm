6a5f2e028b4b551378f96412653ef886206ba691
AJ <735267452@qq.com>
2015-08-17 15:48:12 +0800

    wap all types page

---
+++ m29_wap_qmm/wap-separate-types/index.js
@@ -0,0 +1,33 @@
+$(function () {
+    var left = $('#aj-all-types .aj-left-side'),
+        right = $('#aj-all-types .aj-right-side'),
+        top = $('#top'),
+        scrollTop = -1;
+    console.log($(window).height() - top.height());
+    left.css({'height' : $(window).height() - top.height() + 'px'});
+    left.css({top : top.height() + 'px'});
+    left.find('.aj-li').on('click', function () {
+        $(this).addClass('aj-select').siblings().removeClass('aj-select');
+    });
+    right.css({height : $(window).height() - top.height() + 'px'});
+});
+
+
+
+
+// 演示
+$(function () {
+    $('img[drc]').each(function () {
+        var drc;
+        drc = $(this).attr('drc');
+        var img = new Image(),
+            $this = this;
+        img.src = drc;
+        img.onload = function () {
+            $($this).attr('src', drc).removeAttr('drc');
+        }
+        img.onerro = function () {
+            $($this).attr('src', 'http://www.juanlaoda.com/AdminImageUpload/282030xintup.jpg').removeAttr('drc');
+        }
+    });
+});
