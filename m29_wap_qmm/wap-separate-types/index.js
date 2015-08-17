48f11d0793c3be70331d6746bca37a878023638a
AJ <735267452@qq.com>
2015-08-17 18:08:01 +0800

    all types page style

--- m29_wap_qmm/wap-separate-types/index.js
+++ m29_wap_qmm/wap-separate-types/index.js
@@ -2,12 +2,27 @@ $(function () {
     var left = $('#aj-all-types .aj-left-side'),
         right = $('#aj-all-types .aj-right-side'),
         top = $('#top'),
+        lis,
+        blocks,
+        blocksWrap,
+        i,
+        divEle
         scrollTop = -1;
     if (left.length === 0) return false;
+    lis = left.find('.aj-li');
+    blocksWrap = right.find('.aj-r-inside-wrap');
     left.css({'height' : $(window).height() - top.height() + 'px'});
     left.css({top : top.height() + 'px'});
-    left.find('.aj-li').on('click', function () {
+    for (i = 1; i < lis.length; i++) {
+        divEle = document.createElement('div');
+        $(divEle).addClass('aj-block');
+        $(divEle).html('<div class="newLoading"></div>');
+        blocksWrap.append(divEle);
+    }
+    blocks = blocksWrap.find('.aj-block');
+    lis.on('click', function () {
         $(this).addClass('aj-select').siblings().removeClass('aj-select');
+        blocks.eq($(this).index()).show().siblings().hide();
     });
     right.css({height : $(window).height() - top.height() + 'px'});
 });
@@ -26,7 +41,7 @@ $(function () {
         img.onload = function () {
             $($this).attr('src', drc).removeAttr('drc');
         }
-        img.onerro = function () {
+        img.onerror = function () {
             $($this).attr('src', 'http://www.juanlaoda.com/AdminImageUpload/282030xintup.jpg').removeAttr('drc');
         }
     });
