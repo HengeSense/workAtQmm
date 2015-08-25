1a5d7ad5dc8863d3de94c32024c48c50c7dfb5ab
AJ <735267452@qq.com>
2015-08-25 11:04:48 +0800

    wap search page init

--- m25_custom_event/nodejs/nodeTry/http.js
+++ m25_custom_event/nodejs/nodeTry/http.js
@@ -1,10 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    var name = req.query.name || 'World';
+var http = require('http'),
+    url = require('url');
+var server = http.createServer(function (req, res) {
+    var get = url.parse(req.url, true).query;
     res.writeHead(200,{
         'Content-Type' : 'text/plain'
     });
-    res.end("Hello " + name);
+    res.end("Hello " + get.name);
 }).listen(1337, '127.0.0.1');
 console.log("I am running...");
 
---
+++ m29_wap_qmm/wap-search/search/search.js
@@ -0,0 +1,7 @@
+$(function () {
+    var btn = $('#top .login-wrapper');
+    btn.on('click', function (e) {
+        e.preventDefault();
+
+    });
+});
