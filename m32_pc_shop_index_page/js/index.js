8be90add8902713cff5ac20579d94d5dcefc3da1
AJ <735267452@qq.com>
2015-09-01 14:59:45 +0800

    shop index page of pc

---
+++ m32_pc_shop_index_page/js/index.js
@@ -0,0 +1,99 @@
+$(function () {
+    function Nav(){
+        this.div = $('#aj-top-nav-f-j');
+        this.left = $('#aj-left-side-f-j');
+        this.config();
+        this.fixedTop();
+        this.fenlei();
+        this.gridList();
+    }
+    Nav.prototype = {
+        config : function () {
+            this.fixedTopVal = 32;
+        },
+        fixedTop : function () {
+            var timer,      // for scroll
+                timer2,     // for resize
+                top,
+                left,
+                $this = this;
+            top = $this.div.offset().top;
+            left = $this.div.offset().left;
+            $(window).on('scroll', function() {
+                if (!timer) {
+                    timer = setTimeout(function () {
+                        if (top - $this.fixedTopVal <= $(document.body).scrollTop()) {
+                            $this.div.css({
+                                position : 'fixed',
+                                top : $this.fixedTopVal + 'px',
+                                left : left + 'px',
+                                zIndex : 9999
+                            });
+                        } else {
+                            $this.div.css({position : 'static'});
+                        }
+                        timer = 0;
+                    }, 200);
+                }
+            });
+//            $(window).on('resize', function () {
+//                if (!timer2)  {
+//                    timer2 = setTimeout(function () {
+//                        left = $this.div.offset().left;
+//                        timer2 = 0;
+//                    }, 200)
+//                }
+//            });
+        },
+        fenlei : function () {
+            var aTags = this.div.find('.fenlei .a-tag'),
+                id,
+                aim,
+                prop = {};
+            this.div.on('click', '.fenlei .a-tag', function (e) {
+                e.preventDefault();
+                // style
+                style(this);
+                // scroll
+                scroll(this);
+            });
+            function style(obj) {
+                if ($(this).hasClass('aj-select')) return false;
+                $(aTags).removeClass('aj-select');
+                $(obj).addClass('aj-select');
+            }
+            function scroll(obj) {
+                id = $(obj).attr('href');
+                if (prop[id]) {
+                    aim = prop[id];
+                } else {
+                    aim = $(id);
+                }
+                if (aim) {
+                    $(document.body).animate({
+                        scrollTop : aim.offset().top - 110  + 'px'
+                    });
+                }
+            }
+        },
+        gridList : function (){
+            var aTags = this.div.find('.layout .aj-a'),
+                listClassName = 'aj-grid-to-list-wrap',
+                type; // grid or list
+            aTags.each(function () {
+                if ($(this).hasClass('aj-select')) {
+                    type = $(this).attr('aj-type');
+                    return false;
+                }
+            });
+            // self style
+            this.div.on('click', '.layout .aj-a', function () {
+                $(this).addClass('aj-select').siblings().removeClass('aj-select');
+                type = $(this).attr('aj-type');
+            });
+            // change grid list style
+
+        }
+    };
+    new Nav();
+});
