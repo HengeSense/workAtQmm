867083007d36fcc54b71b90f1029ee364f6eebb4
AJ <735267452@qq.com>
2015-09-11 14:37:45 +0800

    wilddog

---
+++ m25_custom_event/wilddog/js/index.js
@@ -0,0 +1,26 @@
+// https://ajlovechina.wilddogio.com/
+$(function () {
+    var ref = new Wilddog("https://ajlovechina.wilddogio.com/");
+    ref.set({
+        "name" : "Hello World!",
+        "author" : "Wilddog",
+        "location" : {
+            "city" : "beijing",
+            "zip" : 100000
+        }
+    });
+    ref.child("location/city").on("value", function(datasnapshot) {
+        alert(datasnapshot.val());   // 结果会弹出信息"beijing"
+    });
+    var usersRef = ref.child("users");
+    usersRef.set({
+        alanisawesome : {
+            date_of_birth : "June 23, 1912",
+            full_name : "Alan Turing"
+        },
+        gracehop : {
+            date_of_birth: "December 9, 1906",
+            full_name: "Grace Hopper"
+        }
+    });
+});
