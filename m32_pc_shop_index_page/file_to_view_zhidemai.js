770b6231d72273cf840dba313155d932212f3825
AJ <735267452@qq.com>
2015-09-20 12:57:03 +0800

    file_to_view_zhidemai

---
+++ m32_pc_shop_index_page/file_to_view_zhidemai.js
@@ -0,0 +1,5 @@
+//-----  shared目录
+
+// List_BestDealCell.ascx      对应首页值得买渲染视图       div方式     只有精选
+// BestDealItemCell.ascx       对应值得买视图             div方式       包括过期,涨价,下架  div方式
+// listGrid_BestDealCell.ascx  对应值得买视图             ul方式
