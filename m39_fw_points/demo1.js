234c6491e90e737ec2d87f71f7bd28cda6c6b5b3
AJ <735267452@qq.com>
2015-10-09 13:48:07 +0800

    ...

---
+++ m39_fw_points/demo1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by james on 2015/10/9.
+ */
+(function() {
+    var file;
+    file = {
+        'list': [
+            {
+                'uri': "C:\QuanmamaV3\Mvc\apios\zppZdmCategory.ashx",
+                'uri2': "C:\QuanmamaV3\Mvc\apios\zppZdmCategoryOne.ashx"
+            }
+        ]
+    };
+})();
