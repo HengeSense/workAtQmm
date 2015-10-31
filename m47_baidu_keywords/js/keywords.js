b29062c8072b407caf961fc7819b0998593e0ae0
AJ <735267452@qq.com>
2015-10-31 20:00:43 +0800

    jquery in nodejs

--- m47_baidu_keywords/js/keywords.js
+++ m47_baidu_keywords/js/keywords.js
@@ -4,11 +4,16 @@ var results_words = [];
         results = results_words,
         same = {},
         words = "优惠券",
+        query = "http://localhost:520/m47_baidu_keywords/PHP/save.php",
+        body  = $(document.body),
+        num = 0,
         url = "/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=优惠券&rsv_pq=ecd3343b0001a7b8&rsv_t=683eWqxrAYQPQE0aGHiAx4AwguGIK1qorWr2apsy1EMJKaW2%2BEw39xzRiIM&rsv_enter=1&rsv_sug3=14&rsv_sug1=2&rsv_sug2=0&inputT=3913&rsv_sug4=5071";
 
 
-    function ajax(url) {
-        if (results.length > 10) return;
+    function ajax(url, par_word) {
+        if (results.length > 1000) return;
+        if (num % 1000 === 0) {body.html("");}
+        num ++;
         $.ajax({
             url : url,
             type : 'GET',
@@ -16,7 +21,6 @@ var results_words = [];
             async : true,
             success : function (res) {
                 var tds = $(res).find('#rs th a');
-
                 tds.each(function (){
                     var word = $.trim($(this).html()),
                         url = $.trim($(this).attr('href'));
@@ -26,10 +30,14 @@ var results_words = [];
                             'word' : word,
                             'url' : url
                         });
-                        ajax(url);
+                        body.append("<img style='display:none;' src='" + query +"?word=" +
+                                word + "&url=" + url + "'>");
+                        console.log(results_words);
+                        setTimeout(function () {
+                            ajax(url, word);
+                        }, parseInt(Math.random() * 100, 10));
                     }
                 });
-                console.log(results.length);
             },
             error : function () {
 
---
+++ m47_baidu_keywords/node/demo.js
@@ -0,0 +1,7 @@
+//var $ = require('G:/XAMPP/htdocs/Github/nodejs/npm/node_modules/node_modules/jquery');
+
+//console.log("Hello World");
+
+var $ = require('jquery');
+
+console.log($("<div class='div'>123</div>"));
