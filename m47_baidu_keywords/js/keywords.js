c33ef8850d1bfb93a791ea77c2734d501421f543
AJ <735267452@qq.com>
2015-10-31 23:16:19 +0800

    ...

--- m47_baidu_keywords/js/keywords.js
+++ m47_baidu_keywords/js/keywords.js
@@ -1,49 +1,75 @@
-var results_words = [];
+var results_words = [{
+        word : "支付宝",
+        url : "/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=2&ch=&tn=39042058_4_oem_dg&bar=&wd=支付宝&rsv_spt=1&oq=支付宝&rsv_pq=c373930c00018800&rsv_t=7b4daaOzaAt%2BClOT0qv8%2BqAKCnlJgYUCO08C2Lof%2FxWHsG347T2SE5qczj6pkhcPTBiXueKxueI",
+        par : ""
+    }],
+    ok = [],
+    isStop = false;
+
 (function () {
     var root = "https://www.baidu.com/",
-        results = results_words,
         same = {},
-        words = "优惠券",
         query = "http://localhost:520/m47_baidu_keywords/PHP/save.php",
         body  = $(document.body),
         num = 0,
-        url = "/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=优惠券&rsv_pq=ecd3343b0001a7b8&rsv_t=683eWqxrAYQPQE0aGHiAx4AwguGIK1qorWr2apsy1EMJKaW2%2BEw39xzRiIM&rsv_enter=1&rsv_sug3=14&rsv_sug1=2&rsv_sug2=0&inputT=3913&rsv_sug4=5071";
+        nowOne;
 
+    setInterval(function () {
+        if (isStop) return;
+        nowOne = results_words.shift();
+        if (nowOne) {
+            ok.push(nowOne);
+        }
+        if (nowOne && results_words.length < 100) {
+            ajax(nowOne.url, nowOne.word)
+        }
+    }, 1000 / 12);
 
     function ajax(url, par_word) {
-        if (results.length > 1000) return;
-        if (num % 1000 === 0) {body.html("");}
-        num ++;
         $.ajax({
             url : url,
             type : 'GET',
             dataType : 'html',
             async : true,
             success : function (res) {
+                num ++;
                 var tds = $(res).find('#rs th a');
                 tds.each(function (){
                     var word = $.trim($(this).html()),
                         url = $.trim($(this).attr('href'));
                     if (!(word in same)) {
                         same[word] = 1;
-                        results.push({
+                        results_words.push({
                             'word' : word,
-                            'url' : url
+                            'url' : url,
+                            'par' : par_word
                         });
-                        body.append("<img style='display:none;' src='" + query +"?word=" +
-                                word + "&url=" + url + "'>");
-                        console.log(results_words);
-                        setTimeout(function () {
-                            ajax(url, word);
-                        }, parseInt(Math.random() * 100, 10));
+                    } else {
+                        console.log("One Same.");
                     }
                 });
             },
-            error : function () {
-
+            error : function (err) {
+                console.log(err);
             }
         });
     }
 
-    ajax(url);
+    var one;
+    function upload () {
+        one = ok.shift();
+
+        if (!one) return;
+        console.log("正在上传" + one.word);
+        console.log("缓存中还有" + ok.length + "个item");
+
+        body.append("<img style='display:none;' src='" + query +"?word=" +
+            encodeURIComponent(one.word) +
+            "&url=" + encodeURIComponent(one.url) +
+            "&par='" + encodeURIComponent(one.par) + ">");
+    }
+    setInterval(function () {
+        upload();
+    }, 1000 / 12);
+
 })();
